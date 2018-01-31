8895fe5c4ba341d2356e17ffd4011d26eccc48da
Ansonkumar <35687544+Ansonkumar@users.noreply.github.com>
2018-01-31 14:21:47 +0530

    Create anson.c

---
+++ anson.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int c;
+    printf("enter a number c =");
+    scanf("%d",&c);
+    if(c%2==0)
+    printf("The number is even");
+    else
+    printf("The number is odd");
+    }
