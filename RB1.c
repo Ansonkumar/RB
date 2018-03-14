072c6f33964b1afac017ac4f84d503b429a10030
Ansonkumar <35687544+Ansonkumar@users.noreply.github.com>
2018-03-14 16:25:03 +0530

    Update RB1.c

--- RB1.c
+++ RB1.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 void main()
 {
-int a[100],n,i,j;
+int a[100],n, i,j;
 scanf("%d",&n);
-for(i=0;i<n;i++)
+for(i=0; i<n;i++)
 {
-scanf("%d",&a[i]);
+scanf("%d" ,&a[i]);
 }
 for(i=0;i<n;i++)
 {
