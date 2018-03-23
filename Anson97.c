c971f161a9eec02fd2131e25c9fa6a4d412fc941
Ansonkumar <35687544+Ansonkumar@users.noreply.github.com>
2018-03-23 15:52:23 +0530

    Update Anson97.c

--- Anson97.c
+++ Anson97.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
-int main(void) 
-{
-	int a,b,c,result;
-	scanf("%d %d %d",&a,&b,&c);
-	result=((a*b)/c);
-	printf("%d",result);
-	return 0;
+int main(void) {
+int a,b,c,result;
+scanf("%d %d %d",&a,&b,&c);
+result=((a*b)/c);
+printf("%d",result);
+return 0;
 }
